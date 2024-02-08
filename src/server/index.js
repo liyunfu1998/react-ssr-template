f5349bce32102ca9b969acc1ccba5094487a9765
李云富 <liyunfu1998@qq.com>
2024-02-08 05:17:41 +0000

    feat: SSR事件绑定

--- src/server/index.js
+++ src/server/index.js
@@ -3,15 +3,19 @@ import childProcess from 'child_process'
 import {renderToString} from 'react-dom/server'
 import React from 'react'
 import Home from '@/pages/Home'
+import path from 'path'
 
 const app = express()
 const content = renderToString(<Home/>)  // 编译需要渲染的jsx，转换成对应的html
+
+app.use(express.static(path.resolve(process.cwd(), 'client_build')))
 
 app.get('*', (req,res)=>{
   const htmlDOM = `
     <html>
       <body>
         <div id="root">${content}</div>
+        <script src="/index.js"></script>
       </body>
     </html>
   `;
